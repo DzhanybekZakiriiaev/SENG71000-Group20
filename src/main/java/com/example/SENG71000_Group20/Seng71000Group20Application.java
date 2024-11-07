851ea3e637a6ce384c4d8204abfca43d259fd289
Dzhanybek Zakiriiaev <dzhanybek.zakiriiaev@gmail.com>
2024-11-07 13:10:28 -0500

    init

---
+++ src/main/java/com/example/SENG71000_Group20/Seng71000Group20Application.java
@@ -0,0 +1,13 @@
+package com.example.SENG71000_Group20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seng71000Group20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seng71000Group20Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SENG71000_Group20/Seng71000Group20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SENG71000_Group20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seng71000Group20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
